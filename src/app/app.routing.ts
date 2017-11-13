75366b90324d5a376cacc13e3d98469b2d623f33
Gorniv <gornivv@gmail.com>
2017-11-14 00:53:01 +0300

    update modules state

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,7 +2,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: './home/home.module#HomeModule', pathMatch: 'full' },
-  { path: 'material', loadChildren: './materialPage/materialPage.module#MaterialPageModule' },
+  // { path: 'material', loadChildren: './materialPage/materialPage.module#MaterialPageModule' },
   { path: 'primeng', loadChildren: './primengPage/primengPage.module#PrimengPageModule' },
 ];
 
