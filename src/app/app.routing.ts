67579500371cc7b331e897bcdb5f42026d037e59
Gorniv <gornivv@gmail.com>
2017-11-20 15:07:26 +0300

    fix

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', loadChildren: './home/home.module#HomeModule', pathMatch: 'full' },
   { path: 'material', loadChildren: './materialPage/materialPage.module#MaterialPageModule' },
-  { path: 'back', loadChildren: './transfer-back/transfer-back.module#TransferBackModule' },
 ];
 // must use {initialNavigation: 'enabled'}) - for one load page, without reload
 export const AppRoutes = RouterModule.forRoot(routes, { initialNavigation: 'enabled' });
