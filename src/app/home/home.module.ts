55a108d8e69577d10d3303b66c32b6fbe9f5cf89
Dmytro Garastovych <hitpopdimestop@gmail.com>
2018-01-04 11:52:11 +0200

    simple realization of ngx-translate complete

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,11 +2,13 @@ import { HomeRoutes } from './home.routing';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   imports: [
     CommonModule,
-    HomeRoutes
+    HomeRoutes,
+    TranslateModule
   ],
   declarations: [HomeComponent]
 })
--- src/app/transfer-back/transfer-back.module.ts
+++ src/app/transfer-back/transfer-back.module.ts
@@ -2,11 +2,13 @@ import { TransferBackRoutes } from './transfer-back.routing';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TransferBackComponent } from './transfer-back.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   imports: [
     CommonModule,
-    TransferBackRoutes
+    TransferBackRoutes,
+    TranslateModule
   ],
   declarations: [TransferBackComponent]
 })
