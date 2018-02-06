9bcd2989ef9617d783c6fefec03455e0781220ab
Valeriu Vodnicear <vodniciarv@gmail.com>
2018-02-06 22:50:39 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { AppRoutes } from './app.routing';
 import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { TransferHttpModule } from './../modules/transfer-http/transfer-http.module';
+import { TransferHttpModule } from '../modules/transfer-http/transfer-http.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
