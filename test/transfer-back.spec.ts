bd47f49e60986bb7d9ed222a63698c762d07f36a
Gorniv <gornivv@gmail.com>
2018-12-14 16:47:59 +0300

    test: fix timeout

--- test/transfer-back.spec.ts
+++ test/transfer-back.spec.ts
@@ -7,13 +7,11 @@ const url = `http://localhost:4000${page}`;
 
 describe(`test work http request ${page} with ssr`, () => {
   it('Should be exist "total_pages"', async () => {
-    this.timeout(5000);
     body = await request(url);
     return expect(body.includes('total_pages')).to.equal(true);
   });
 
   it('Should be exist "createdAt"', async () => {
-    this.timeout(5000);
     body = await request(url);
     expect(body.includes('createdAt')).to.equal(true);
   });
