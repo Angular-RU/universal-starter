f051e5ab3176987b53b60a9285a43f816c7e16db
Gorniv <gornivv@gmail.com>
2019-04-26 15:20:55 +0300

    test: use auth for test

--- test/request-service.js
+++ test/request-service.js
@@ -1,12 +1,15 @@
 const request = require('request');
 
-module.exports = async value =>
+module.exports = async (value) =>
   new Promise((resolve, reject) => {
-    request.get(value, { proxy: '' }, (error, response, data) => {
-      if (error) {
-       console.log(error);
-        reject(error);
-      }
-      else resolve(data);
-    });
+    request.get(
+      value,
+      { proxy: '', headers: { Cookie: 'token=mock' } },
+      (error, response, data) => {
+        if (error) {
+          console.log(error);
+          reject(error);
+        } else resolve(data);
+      },
+    );
   });
